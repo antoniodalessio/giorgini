b689fc00e524ec70f627c8ce33c112c63b56621b
Antonio D'Alessio <antonio@adias.it>
2020-04-20 19:31:26 +0200

    3

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var express = require('express')
 var expressApp = express();
 
 expressApp.use(express.json());
-expressApp.listen(3000, () => {
-    console.log('Server running on port 3000!');
+expressApp.listen(8124, () => {
+    console.log('Server running on port 8124!');
 });
 
 expressApp.get('/', async (req, res) => {
-
+    res.end('Hello World\n');
 })
