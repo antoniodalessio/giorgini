6c9b97093e612adf81ce8da00275d0da698053b1
Antonio D'Alessio <antonio@adias.it>
2020-04-22 15:21:29 +0200

    ...

--- src/index.ts
+++ src/index.ts
@@ -24,10 +24,10 @@ expressApp.get('/', async (req, res) => {
                 "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
     var result = template(data);
 
-    fs.writeFile('/tmp/helloworld.html', result, function (err) {
+    fs.writeFile('/mnt/helloworld.html', result, function (err) {
         if (err) return console.log(err);
         
-        clientftp.upload('/tmp/helloworld.html', './www.amaliacardo.it/test/helloworld.html', 755);
+        clientftp.upload('/mnt/helloworld.html', './www.amaliacardo.it/test/helloworld.html', 755);
         
     });
 
