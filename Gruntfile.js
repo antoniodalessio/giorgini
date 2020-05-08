e641bbc51e5c9c894f3c125db9911252766003e7
Antonio D'Alessio <antonio@adias.it>
2020-05-08 21:08:01 +0200

    add description to template

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+module.exports = function(grunt) {
+
+	var baseDir = './site/'
+
+	var bowerDir = "bower_components/",
+		common = [
+			bowerDir + 'jquery/dist/jquery.js',
+			bowerDir + 'magnific-popup/dist/jquery.magnific-popup.js',
+			baseDir + 'js/common.js'
+		];
+
+
+	grunt.initConfig({
+		
+		compass: {
+			dist: {
+			options: {
+				sassDir: baseDir + 'scss',
+				cssDir: baseDir + 'css',
+				environment: 'production'
+			}
+			},
+			dev: {
+				options: {
+					sassDir: baseDir + 'scss',
+					cssDir: baseDir + 'css'
+				}
+			}
+		},
+		uglify: {
+			options: {
+				mangle: false
+			},
+			my_target: {
+				files: {
+					[baseDir + 'js/index.min.js']: common.concat([baseDir + 'js/index.js']),
+					[baseDir + 'js/about.min.js']: common.concat([baseDir + 'js/about.js']),
+					[baseDir + 'js/what.min.js']: common.concat([baseDir + 'js/what.js']),
+					[baseDir + 'js/product.min.js']: common.concat([baseDir + 'js/product.js']),
+					[baseDir + 'js/work.min.js']: common.concat([baseDir + 'js/work.js']),
+				}
+			}
+		},
+		webfont: {
+			icons: {
+				options: {
+				stylesheet: 'scss',
+				relativeFontPath: '../fonts'
+			},
+			src: baseDir + 'svg/*.svg',
+			dest: baseDir + 'fonts',
+			destCss: baseDir + 'scss/'
+			}
+		},
+		watch: {
+			css: {
+				files: [baseDir + 'scss/**/*.scss'],
+				tasks: ['compass'],
+				options: {
+				spawn: false,
+				},
+			},
+			js: {
+				files: [baseDir + 'js/**/*.js'],
+				tasks: ['uglify'],
+				options: {
+				spawn: false,
+				},
+			},
+			webfont: {
+				files: [baseDir + 'svg/**/*.svg'],
+				tasks: ['webfont'],
+				options: {
+				spawn: false,
+				},
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-webfont');
+
+
+	grunt.registerTask('default', ['compass', 'uglify', 'webfont', 'watch']);
+
+}
--- readme.md
+++ readme.md
@@ -7,8 +7,25 @@ mongodump -d amaliacardo -o ./dump
 /Users/antoniodalessio/Desktop/workspace/amaliacardo-admin/dump/amaliacardo
 
 
-scp -r /Users/antoniodalessio/Desktop/workspace/amaliacardo-admin/dump/amaliacardo antonio.root@2a7aa4385a3e:/mnt/dump/amaliacardo
+scp -r /Users/antoniodalessio/Desktop/workspace/amaliacardo-admin/dump/amaliacardo antonio.6263c5b0847fb8966011d5fe0838835c10538df998e18cfa20b419a810173057c87438@devshell.cloudno.de:/mnt/dump/amaliacardo
+
+
+scp -r /Users/antoniodalessio/Desktop/workspace/amaliacardo-admin/dump/amaliacardo /mnt/dump
 
 
 Your API token is 6HZ2ln8VUT - keep it secrect!
 Your CouchDB/MongoDB token is 1QyxqMm0rC - keep it secrect!
+
+
+
+ssh -L27017:localhost:27017 6263c5b0847fb8966011d5fe0838835c10538df998e18cfa20b419a810173057c87438@devshell.cloudno.de:27017
+
+
+mongodump -h mongodb_1:27017 -d amaliacardo -u antonio.dalessio -p 1QyxqMm0rC -o amaliacardo
+
+// import dump funzionante
+// passare prima i files con filezilla
+mongorestore -h mongodb_1:27017 -d amaliacardo -u antonio.dalessio -p 1QyxqMm0rC /mnt/dump
+
+
+http://amaliacardo.cloudno.de/authenticate
