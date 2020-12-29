c5a1c753975584e6605710a505afa13f02539e62
Antonio <antonio@adias.it>
2020-12-29 21:29:12 +0100

    adding images to services

--- dist/controllers/service.controller.js
+++ dist/controllers/service.controller.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,6 +18,22 @@ class ServiceController extends base_controller_1.default {
     constructor() {
         super();
         this.model = models_1.Service;
+    }
+    getAll(req, res) {
+        const _super = Object.create(null, {
+            getAll: { get: () => super.getAll }
+        });
+        return __awaiter(this, void 0, void 0, function* () {
+            yield _super.getAll.call(this, req, res, 'images');
+        });
+    }
+    get(req, res) {
+        const _super = Object.create(null, {
+            get: { get: () => super.get }
+        });
+        return __awaiter(this, void 0, void 0, function* () {
+            yield _super.get.call(this, req, res, 'images');
+        });
     }
 }
 exports.default = ServiceController;
--- src/controllers/service.controller.ts
+++ src/controllers/service.controller.ts
@@ -10,6 +10,14 @@ class ServiceController extends BaseController{
     this.model = Service
   }
 
+  async getAll(req: any, res: any) {
+    await super.getAll(req, res, 'images')
+  }
+
+  async get(req: any, res: any) {
+    await super.get(req, res, 'images')
+  }
+
 }
 
 export default ServiceController;
